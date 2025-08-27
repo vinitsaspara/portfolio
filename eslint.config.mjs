97e9d9e8a4d9e94138bb440d0f52e9a0b364de70
vinitsaspara <sasparavinit@gmail.com>
2025-08-27 18:16:31 +0530

    Update ESLint configuration to disable specific React and TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
